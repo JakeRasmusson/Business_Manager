da1412507484ce908962e6dcb84b6443ffa9c61f
Jake Rasmusson <jakerasmusson@gmail.com>
2024-05-15 18:03:32 -0500

    Fixes recursive function and data display

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const  inquirer = require('./node_modules/inquirer')
 const { mainMethods, mainQuestions } = require('./lib/modules/questions.js')
 const inquirerHelper = require('./lib/modules/inquirer.js')
 
 async function questionsAsker(){
     const answers = await inquirerHelper(mainQuestions)
     const operation = answers.main
-    mainMethods[operation]()
+    await mainMethods[operation]()
+    await questionsAsker()
 }
 
 function init(){
-    console.log('Welcome to the magical world of tomorrow!')
+    console.log(`
+
+
+    
+Welcome to the magical world of tomorrow!
+    
+    
+    
+    
+    `)
     questionsAsker()
 }
 
 
 
-
-
-module.exports =  questionsAsker
 init()
 
 
